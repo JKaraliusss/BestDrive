2ed7fa7bbbdaea51111b8da7b7bc8f2758442e45
JKaraliusss <juozaskaralius21@gmail.com>
2023-03-25 00:37:52 +0200

    sukurtas kalendorius

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Profile')
+
+@section('content')
+    @include('profile.partials.update-profile-information-form')
+    @include('profile.partials.update-password-form')
+    @include('profile.partials.delete-user-form')
+@endsection
+
